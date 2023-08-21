68910dee8b4490a0fe157ecf25bfc71613e28ba4
Arif Hossin || Full Stack Developer <46327525+webdevarif@users.noreply.github.com>
2023-08-22 01:22:07 +0600

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,3 +47,29 @@
 
     });
 }(jQuery));
+
+
+
+function updateTimer() {
+	future  = Date.parse("October 11, 2023 11:30:00");
+	now     = new Date();
+	diff    = future - now;
+	
+	days  = Math.floor( diff / (1000*60*60*24) );
+	hours = Math.floor( diff / (1000*60*60) );
+	mins  = Math.floor( diff / (1000*60) );
+	secs  = Math.floor( diff / 1000 );
+	
+	d = days;
+	h = hours - days  * 24;
+	m = mins  - hours * 60;
+	s = secs  - mins  * 60;
+	
+	document.getElementById("timer")
+		.innerHTML =
+		'<div>' + d + '<span> ימים</span></div>' +
+		'<div>' + h + '<span> שעה (ות</span></div>' +
+		'<div>' + m + '<span> דקות</span></div>' +
+		'<div>' + s + '<span> שניות</span></div>' ;
+}
+setInterval('updateTimer()', 1000 );
